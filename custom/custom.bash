a20fa2c3878dfe88fcf61b9f33146f5b2d39a18a
Robin Wenglewski <robin@wenglewski.de>
2015-10-11 10:39:14 +0200

    add custom/custom.bash and modify existing docker aliases

---
+++ custom/custom.bash
@@ -0,0 +1,24 @@
+# export PATH="$PATH:~/.bin"
+
+alias e="$EDITOR"
+alias e.="$EDITOR ."
+alias c="cd ~/Code"
+alias rm="/usr/local/bin/trash"
+alias grm="git clean -f; git reset --hard"
+alias idea='open -a "IntelliJ IDEA 15" .'
+alias gca="git add .; git commit -v"
+
+alias psg="ps -ef|grep"
+
+# direnv
+eval "$(direnv hook $0)"
+
+alias updatedb="sudo /usr/libexec/locate.updatedb"
+
+# kill pid that listens on given port
+# e.g. kport 3000
+function kport {
+	PID_FOUND=`lsof -i :$1|awk '{ print $2 }'| awk 'NR==2'`
+	echo "killing PID: $PID_FOUND"
+	kill ${PID_FOUND}
+}
