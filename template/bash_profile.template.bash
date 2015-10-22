93a1c3364baf1e5e8c3db91792f3d37d80eb81b5
Robin Wenglewski <robin@wenglewski.de>
2015-10-22 18:52:26 +0200

    clean up profile

--- template/bash_profile.template.bash
+++ template/bash_profile.template.bash
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
-
 # Path to the bash it configuration
 export BASH_IT="$HOME/.bash_it"
 
 # Lock and Load a custom theme file
 # location /.bash_it/themes/
 export BASH_IT_THEME='bakke'
-
-# Your place for hosting Git repos. I use this for private repos.
-export GIT_HOSTING='git@git.domain.com'
 
 # Set my editor and git editor
 export EDITOR="subl"
@@ -26,10 +22,6 @@ export TODO="t"
 
 # Set this to false to turn off version control status checking within the prompt for all themes
 export SCM_CHECK=true
-
-# Set vcprompt executable path for scm advance info in prompt (demula theme)
-# https://github.com/xvzf/vcprompt
-#export VCPROMPT_EXECUTABLE=~/.vcprompt/bin/vcprompt
 
 # Load Bash It
 source $BASH_IT/bash_it.sh
