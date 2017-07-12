1e2fb0949912bee9825f72ef6b3df74b6ea6c928
Robin Wenglewski <robin@wenglewski.de>
2017-07-12 21:18:06 +0200

    add nvm to bash profile

--- template/bash_profile.template.bash
+++ template/bash_profile.template.bash
@@ -67,3 +67,9 @@ export SCM_CHECK=true
 source $BASH_IT/bash_it.sh
 
 test -e "${HOME}/.iterm2_shell_integration.bash" && source "${HOME}/.iterm2_shell_integration.bash"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
